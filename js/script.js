6c7ebc208d57f4a33f7d38ac5c8eb93b047b959c
ilhamn45 <101439169+ilhamn45@users.noreply.github.com>
2025-05-25 09:14:35 +0700

    Update js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 const c = document.getElementById("Celsius");
 
 function konversiToFahrenheit() {
-    const f = ((c.value * 1.8) + 32)
-    document.getElementById("HasilFahrenheit").textContent = f
+  if (c.value.length > 1 && c.value.startsWith("0")) {
+    document.getElementById("HasilFahrenheit").textContent = "Input tidak valid";
+    // const inputValid = document.createElement("p");
+    // inputValid.textContent = "Input tidak valid";
+    // document.body.appendChild(inputValid);
+    // document.getElementById("HasilFahrenheit").appendChild(inputValid);
+    return;
+  } else {
+    const f = c.value * 1.8 + 32;
+    document.getElementById("HasilFahrenheit").textContent = f;
+  }
 }
 
 function reset() {
-    document.getElementById("Celsius").value = ""
-    document.getElementById("HasilFahrenheit").textContent = ""
+  c.value = "";
+  document.getElementById("HasilFahrenheit").textContent = "";
 }
 
 // c.addEventListener("input", function () {
